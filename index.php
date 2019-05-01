ca1d3a718204f6fc7d386fd3c23776ddc4df616e
Paulo Roberto Borges <paulorborges@yahoo.com.br>
2019-05-01 17:42:12 -0300

    Configuracao classe page

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php 
-
+/* busca as dependencias do projeto */
 require_once("vendor/autoload.php");
-
-$app = new \Slim\Slim();
+/* classes necessarias */
+use \Slim\Slim;
+use \Hcode\Page;
+/* criacao do objeto para utilização de rotas */
+$app = new Slim();
 
 $app->config('debug', true);
-
+/* metodo para trabalhar com as rotas */
 $app->get('/', function() {
     
+    $page = new Page();
+    $page->setTpl("index");
 	//echo "OK";
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	//$sql = new Hcode\DB\Sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+	//echo json_encode($results);
 });
-
+/* inicia a montagem da pagina */
 $app->run();
 
- ?>
+?>
---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,42 @@
+<?php
+	namespace Hcode;
+	use Rain\Tpl;
+	class Page{
+		private $tpl;
+		private $options = [];
+		private $defaults = [
+			"data"=>[]
+		];
+		private function setData ($data = array()){
+			foreach ($data as $key => $value){
+				$this->tpl->assign($key,$value);
+			}
+		}
+		//Método construtor
+		public function __construct($opts = array()){
+			/* no Merge é importante a ordem, no formato abaixo, os conflitos entre o opts e defaults, vale o opts*/
+			$this->options=array_merge($this->defaults,$opts);
+			// config
+			$config = array(
+				"tpl_dir" => $_SERVER["DOCUMENT_ROOT"]."/views/",
+				"cache_dir" => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+				"debug" => true // set to false to improve the speed
+			 );
+
+			Tpl::configure( $config );
+			$this->tpl = new Tpl;
+			$this->setData($this->options["data"]);
+			$this->tpl->draw("header");
+		}
+
+		/* método para corpo da página */
+		public function setTpl($name, $data = array(),$returnHTML = false){
+			$this->setData($data);
+			return $this->tpl->draw($name,$returnHTML);
+		}
+		//Método destrutor
+		public function __destruct(){
+			$this->tlp->draw("footer");
+		}
+	}
+?>
