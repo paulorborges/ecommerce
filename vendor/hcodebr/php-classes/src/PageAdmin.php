69fad7587a32fc7775adfdf485e5a6c5be2bbb90
Paulo Roberto Borges <paulorborges@yahoo.com.br>
2019-05-05 22:01:03 -0300

    Template admin com erro

--- vendor/hcodebr/php-classes/src/PageAdmin.php
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -3,7 +3,8 @@
 	//use Rain\Tpl;
 	class PageAdmin extends Page{
 		public function __construct($opts = array(), $tpl_dir = "/ecommerce/views/admin/"){
-			/* para evitar de refazer todos os métodos da classe pai, utiliza-se o parent para utilizar o método propriamente dito (da classe pai)*/
+			/* para evitar refazer todos os métodos da classe pai, utiliza-se o parent 
+			para utilizar o método propriamente dito (da classe pai)*/
 			parent:: __construct($opts, $tpl_dir);
 		}
 	}
